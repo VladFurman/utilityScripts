2fb504ec826547d04d28b6f50c80264a123e6957
VladFurman <vfurman@ucsc.edu>
2023-02-23 16:15:17 -0800

    Working bed reminder script.

--- bedReminder.ps1
+++ bedReminder.ps1
@@ -1,17 +1,7 @@
 ﻿(New-Object -ComObject shell.application).toggleDesktop()
 
-$MsgText = "Is what you're doing more important than rest?"
-$MsgTitle = "title"
-
-#[System.Windows.MessageBox]::Show($message)
-
-
-param (
-        [Parameter(Mandatory=$true)]
-          [String]$MsgTitle,
-        [Parameter(Mandatory=$true)]
-          [String]$MsgText
-)
+$MsgText = "Is what you're doing more important than rest?`r80% energy levels are optimal."
+$MsgTitle = "Consider Rest"
 
 Add-Type -AssemblyName Microsoft.VisualBasic 
 
@@ -22,7 +12,7 @@ public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
 “@ -name “Win32ShowWindowAsync” -namespace Win32Functions –passThru
 
 function Show-PowerShell() { 
-     [void]$showWindowAsync::ShowWindowAsync((Get-Process –id $pid).MainWindowHandle, 10) 
+    [void]$showWindowAsync::ShowWindowAsync((Get-Process –id $pid).MainWindowHandle, 10) 
 }
 
 function Hide-PowerShell() { 
@@ -31,13 +21,16 @@ function Hide-PowerShell() {
 
 # ------------------ End Helper -------------------------------
 
-
 Clear-Host
 
 Hide-PowerShell
 
+$styleEnum = [Microsoft.VisualBasic.MsgBoxStyle]
+
+$myStyle = $styleEnum::Question + $styleEnum::YesNo + $styleEnum::MsgBoxSetForeground + $styleEnum::SystemModal
+
 $MsgBox = [Microsoft.VisualBasic.Interaction]
 
-[void] $MsgBox::MsgBox($MsgText,0,$MsgTitle)
+[void] $MsgBox::MsgBox($MsgText, $myStyle, $MsgTitle)
 
 Show-PowerShell
